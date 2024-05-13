8d598a6876a16181e9051bd8947c3fedf3b81fc0
Mohamed Amine Amazzal <amazzalhusa@gmail.com>
2024-05-13 21:36:27 +0100

    gg

--- srcs/requirments/mariadb/create_db.sh
+++ srcs/requirments/mariadb/create_db.sh
@@ -5,13 +5,13 @@ COLOR_RESET='\033[0m'
  
 #'%' is a wildcard that matches any host, 0.0.0.0
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}'; \
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}; \
-    CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}'; \
-    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%'; \
-    FLUSH PRIVILEGES;" > /tmp/db.sql
-
-mariadbd --user=root --bootstrap < /tmp/db.sql
+echo "\
+FLUSH PRIVILEGES; \
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}'; \
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}; \
+CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}'; \
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%'; \
+FLUSH PRIVILEGES;" | mariadbd --user=mysql --bootstrap --verbose=0
 
 echo -e "📖 ${COLOR_YELLOW}ALL DONE ${COLOR_RESET}"
 exec "$@"
