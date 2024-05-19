bc154b0d905584ea4f577ab8797a3c60b4f76eef
mamazzal <amazzalhusa@gmail.com>
2024-05-19 13:35:41 -0400

    g

--- srcs/requirements/nginx/indexs/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    echo "HELLO , THIS IS A PHP FILE\n";
-    phpinfo();
-?>
---
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
+define('DB_HOST', 'mysql');
+
+?>
