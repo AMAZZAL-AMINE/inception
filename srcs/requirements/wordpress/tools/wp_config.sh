2b39d26a3d40837fbaff43c1488a8d64725c4738
mamazzal <amazzalhusa@gmail.com>
2024-05-31 11:03:58 -0400

    gg

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
+# check if wordpress is already installed
+if ! [ -f /var/www/wp-config.php ]; then
+    wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" \
+        --dbpass="${DB_PASSWORD}"  --path=/var/www --allow-root \
+        --skip-check --dbhost=mariadb
 
-echo "DB-NAME: ${DB_NAME}"
-echo "DB-USER: ${DB_USER}"
-echo "DB-PASSWORD: ${DB_PASSWORD}"
-echo "WP-USER: ${WP_USER}"
+    # for welcome page
+    wp core install --url="${WP_URL}" --title="${WP_SITE_TITLE}" \
+        --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" --path=/var/www --allow-root
 
-wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" \
-    --dbpass="${DB_PASSWORD}" --path=/var/www --allow-root \
-    --skip-check --dbhost=mariadb
-
-# for welcome page
-wp core install --url="${WP_URL}" --title="${WP_SITE_TITLE}" \
-    --admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASSWORD}" \
-    --admin_email="${WP_ADMIN_EMAIL}" --path=/var/www --allow-root
-
-# for wp user
-wp user create "${WP_USER}" "${WP_USER_EMAIL}" --role=author \
-    --user_pass="${WP_USER_PASSWORD}" --path=/var/www --allow-root
-
+    # for wp user
+    wp user create "${WP_USER}" "${WP_USER_EMAIL}" --role=author \
+        --user_pass="${WP_USER_PASSWORD}" --path=/var/www --allow-root
+fi
 exec "$@"
