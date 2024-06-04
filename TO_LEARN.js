dd40b627ee04d82251513570a5a314ab3e8feb95
mamazzal <amazzalhusa@gmail.com>
2024-06-04 07:38:54 -0400

    chanfed

--- TO_LEARN.js
+++ TO_LEARN.js
@@ -1,3 +1,15 @@
 // [DONE] => learn about namespaces and cgroups in docker and  PID1;
-// [] => learn about docker-networking;
-// [] => learn about docker-volumes;
+// [DONE] => learn about docker-networking;
+// [DONE] => learn about docker-volumes;
+// [] => learn about ssl and how it work and what are ssl certificates; 
+// => {
+    // driver_opts:
+    // type: none
+    // o: bind
+    // device: /home/mamazzal/data/mariadb
+// }
+
+
+// How Docker and docker compose work
+// The difference between a Docker image used with docker compose and without docker compose
+// The benefit of Docker compared to VMs
